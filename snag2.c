50151b56fe35d739b23c832ca0fb0904ba68e944
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-06-19 23:24:38 +1000

    Update snag2.c

--- snag2.c
+++ snag2.c
@@ -28,8 +28,8 @@ int convertPixel16(uint16_t pxl) {
     uint8_t gray = 
         (0.299 * r +
          0.587 * g +
-         0.114 * b)
-    return gray
+         0.114 * b);
+    return gray;
 }
 
 int main() {
